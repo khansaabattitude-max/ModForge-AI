99b1a33063a1906d11bbb2e25f42c0cf6105f385
khansaabattitude-max <khansaabattitude@gmail.com>
2025-10-29 15:56:59 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
+    "./src/pages/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}"
   ],
   theme: {
-    extend: {
-      colors: {
-        'mc-dark': '#1A1A1A',
-        'mc-gray': '#2C2C2C',
-        'mc-light-gray': '#3A3A3A',
-        'mc-green': '#39FF14',
-        'mc-blue': '#3B82F6',
-      },
-    },
+    extend: {},
   },
   plugins: [
-    require('@tailwindcss/typography'),
+    require('@tailwindcss/typography')
   ],
-};
+}
+
