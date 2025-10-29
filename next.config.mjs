7e22f9dd4921a548e7645b2acbe3d3d0febe1ef4
khansaabattitude-max <khansaabattitude@gmail.com>
2025-10-29 15:56:22 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import { defineConfig } from 'next';
+
+const nextConfig = defineConfig({
   reactStrictMode: true,
-};
+  swcMinify: true
+});
 
 export default nextConfig;
