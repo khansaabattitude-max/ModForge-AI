b50337d56f0b8f4291556ab7fdcce54083deb979
khansaabattitude-max <khansaabattitude@gmail.com>
2025-10-29 21:00:26 +0530

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
+import '../styles/globals.css';
 import React from 'react';
 import { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    // Yeh Tailwind classes body ko dark background dengi
+    <div className="bg-gray-900 text-white min-h-screen"> 
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
