8bbb050cb337a21ac6712963f9c8860b3222b6b2
khansaabattitude-max <khansaabattitude@gmail.com>
2025-10-29 17:55:54 +0530

    Update and rename next.config.mjs to next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// next.config.js (File renamed to .js and using CommonJS format)
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Yeh 'output: standalone' line Next.js ko Vercel par sahi se deploy hone mein madad karti hai
+  output: 'standalone', 
+};
+
+module.exports = nextConfig;
