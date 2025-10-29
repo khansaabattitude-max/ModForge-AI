529ddd3528f43a300022bb933e5fad1a1483988b
khansaabattitude-max <khansaabattitude@gmail.com>
2025-10-29 17:29:56 +0530

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,22 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
+// Yeh file Next.js mein har page ke base HTML structure ko define karti hai.
+
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/*
+          Google Search Console ka Verification Tag Yahan Daal Diya Gaya Hai.
+        */}
         <meta name="google-site-verification" content="DoZI_GopcOg-q6fim50D5Wd62MXhg5_s_tXbGsOrV3M" />
+        
+        {/*
+          Agar aapke paas koi aur zaroori tags hain (jaise favicon ya fonts), woh yahan aayenge.
+        */}
+        
       </Head>
-      <body className="bg-mc-dark text-white">
+      <body>
         <Main />
         <NextScript />
       </body>
